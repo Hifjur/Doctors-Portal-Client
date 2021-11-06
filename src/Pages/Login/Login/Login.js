28ae0a6ad35c2aa3fbc6a1f60c2506693b8d8d33
HIFJUR RAHMAN <hifjur1313@gmail.com>
2021-11-06 19:23:32 +0600

    private route

--- src/Pages/Login/Login/Login.js
+++ src/Pages/Login/Login/Login.js
@@ -1,12 +1,15 @@
 import { Alert, Button, CircularProgress, Container, Grid, TextField, Typography } from '@mui/material';
 import React, { useState } from 'react';
-import { NavLink } from 'react-router-dom';
+import { NavLink, useLocation, useHistory } from 'react-router-dom';
 import useAuth from '../../../Hooks/useAuth';
 import login from '../../../images/login.png'
 
 const Login = () => {
     const [loginData, setLoginData] = useState({});
     const { user, loginUser, isLoading, error } = useAuth();
+
+    const location = useLocation();
+    const history = useHistory();
     const handleOnChange = e => {
         const field = e.target.name;
         const value = e.target.value;
@@ -15,7 +18,7 @@ const Login = () => {
         setLoginData(newLoginData);
     }
     const handleLoginSubmit = e => {
-        loginUser(loginData.email, loginData.password);
+        loginUser(loginData.email, loginData.password, location, history);
         e.preventDefault();
     }
     return (
